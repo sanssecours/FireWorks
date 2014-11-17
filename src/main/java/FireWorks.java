945b27de214f6e558c1539ae2cb33f0398a74825
René Schwaiger <sanssecours@f-m.fm>
2014-11-17 21:45:17 +0100

    Add initial code to write and read from space

--- src/main/java/FireWorks.java
+++ src/main/java/FireWorks.java
@@ -1,4 +1,13 @@
+import org.mozartspaces.core.Capi;
+import org.mozartspaces.core.ContainerReference;
+import org.mozartspaces.core.DefaultMzsCore;
+import org.mozartspaces.core.Entry;
+import org.mozartspaces.core.MzsCore;
+import org.mozartspaces.core.MzsCoreException;
 import org.slf4j.Logger;
+
+import java.util.ArrayList;
+
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class FireWorks {
@@ -6,7 +15,20 @@ public class FireWorks {
     private static final Logger slf4jLogger = getLogger(FireWorks.class);
 
     public static void main(String arguments[]) {
-        System.out.println("Hello fast Space!");
-        slf4jLogger.debug("Hello");
+        ContainerReference wood = null;
+        MzsCore mozartSpace = DefaultMzsCore.newInstance();
+        Capi capi = new Capi(mozartSpace);
+        ArrayList<String> result;
+
+        try {
+            wood = capi.createContainer();
+            capi.write(wood, new Entry("Test"));
+            result = capi.read(wood);
+            System.err.println("Read: " + result.toString());
+            capi.destroyContainer(wood, null);
+        } catch(MzsCoreException e) {
+            e.printStackTrace();
+        }
+        mozartSpace.shutdown(true);
     }
 }
