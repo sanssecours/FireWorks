b99c4a904d5d55bf3bd8c95cd4c661a3b505346b
Johannes Eder <jo.eder@gmx.at>
2014-11-21 03:02:25 +0100

    Alle Spalten sind editierbar Typenspalte zu einem Dropdown menue umgeformt

--- src/main/java/org/falafel/GUIController.java
+++ src/main/java/org/falafel/GUIController.java
@@ -6,14 +6,27 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.TextFieldTableCell;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Created by Johannes on 21.11.2014.
  */
 public class GUIController {
+    /** Get the Logger for the current class. */
+    private static final Logger LOGGER = getLogger(FireWorks.class);
     /**  The data as an observable list for SupplyOrder */
     private ObservableList<SupplyOrder> Order = FXCollections.observableArrayList();
+
+    private ObservableList<String> typesChoiceList = FXCollections.observableArrayList (
+            new String("Casing"),
+            new String("Effect"),
+            new String("Propellant"),
+            new String("Wood")
+    );
 
     @FXML
     private TableView<SupplyOrder> supplyTable;
@@ -32,7 +45,7 @@ public class GUIController {
      */
     @FXML
     private void initialize() {
-        Order.add(new SupplyOrder("Name", "Type", 0, 0));
+        Order.add(new SupplyOrder());
 
         supplyTable.isEditable();
 
@@ -42,7 +55,7 @@ public class GUIController {
         supplierNameColumn.isEditable();
 
         orderedTypeColumn.setCellValueFactory(cellData -> cellData.getValue().typeProperty());
-        orderedTypeColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        orderedTypeColumn.setCellFactory(ComboBoxTableCell.forTableColumn(typesChoiceList));
         orderedTypeColumn.isEditable();
 
         orderedQuantityColumn.setCellValueFactory(cellData -> cellData.getValue().quantityProperty());
@@ -66,10 +79,16 @@ public class GUIController {
      */
     @FXML
     private void startSuppliers(final ActionEvent event) {
+        SupplyOrder nextOrder;
         Supplier supplier;
         final int numberOfSuppliers = 1;
+        if(!Order.isEmpty()) {
+            nextOrder = Order.remove(0);
+            LOGGER.debug(nextOrder.toString());
+        } else
+            System.out.println("No new order!");
 
-        Order.remove(0);
+
 //        for (int supplierId = 1;
 //             supplierId <= numberOfSuppliers;
 //             supplierId++) {
@@ -80,12 +99,26 @@ public class GUIController {
     }
 
     public void setSupplierName(TableColumn.CellEditEvent<SupplyOrder, String> stCellEditEvent) {
-        stCellEditEvent.getTableView().getItems().get(stCellEditEvent.getTablePosition().getRow()).setSupplierName(stCellEditEvent.getNewValue());
-
-        System.out.println("Editable Column " + supplierNameColumn.getCellData(0).toString());
+        stCellEditEvent.getTableView().getItems().get(stCellEditEvent.getTablePosition().getRow()).setSupplierName(
+                stCellEditEvent.getNewValue());
     }
 
     public void NewOrder(ActionEvent actionEvent) {
-        Order.add(new SupplyOrder("Name", "Type", 0, 0));
+        Order.add(new SupplyOrder());
+    }
+
+    public void setType(TableColumn.CellEditEvent<SupplyOrder, String> stCellEditEvent) {
+        stCellEditEvent.getTableView().getItems().get(stCellEditEvent.getTablePosition().getRow()).setType(
+                stCellEditEvent.getNewValue());
+    }
+
+    public void setQuantity(TableColumn.CellEditEvent<SupplyOrder, String> stCellEditEvent) {
+        stCellEditEvent.getTableView().getItems().get(stCellEditEvent.getTablePosition().getRow()).setQuantity(
+                Integer.parseInt(stCellEditEvent.getNewValue()));
+    }
+
+    public void setQuality(TableColumn.CellEditEvent<SupplyOrder, String> stCellEditEvent) {
+        stCellEditEvent.getTableView().getItems().get(stCellEditEvent.getTablePosition().getRow()).setQuality(
+                Integer.parseInt(stCellEditEvent.getNewValue()));
     }
 }
