925f997b75264b2b16c5e4db2891c8478b1bd157
René Schwaiger <sanssecours@f-m.fm>
2014-11-26 16:19:21 +0100

    Update documentation of `Effect`

--- src/main/java/org/falafel/Effect.java
+++ src/main/java/org/falafel/Effect.java
@@ -9,7 +9,7 @@ public class Effect extends Material {
     private boolean defect;
 
     /**
-     * Create new propellent package with the given attributes.
+     * Create new effect with the given attributes.
      *
      * @param identifier
      *          A integer value that (uniquely) identifies this material.
@@ -21,15 +21,15 @@ public class Effect extends Material {
      *          A boolean value specifying if this effect is defect or not
      */
     public Effect(final int identifier, final String supplierName,
-                      final int supplierId, final boolean defect) {
+                  final int supplierId, final boolean defect) {
         super(identifier, supplierName, supplierId);
         this.defect = defect;
     }
 
     /**
-     * Return the string representation of the propellent package.
+     * Return the string representation of the effect.
      *
-     * @return A string containing properties of this propellent package
+     * @return A string containing properties of this effect charge
      */
     public final String toString() {
         return "Effect: " + super.toString() + " -- Defect: " + defect;
