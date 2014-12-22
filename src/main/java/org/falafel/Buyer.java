a5df0a2949afab63afcc21052d0946c4de2c3dbd
René Schwaiger <sanssecours@f-m.fm>
2014-12-22 16:09:05 +0100

    Add code to clear purchase table in `Buyer`

--- src/main/java/org/falafel/Buyer.java
+++ src/main/java/org/falafel/Buyer.java
@@ -166,6 +166,7 @@ public final class Buyer extends Application {
      */
     @SuppressWarnings("unused")
     public void clearPurchase(final ActionEvent actionEvent) {
+        purchases.clear();
     }
 
     /**
