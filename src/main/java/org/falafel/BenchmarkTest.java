78aac57810d49c344e3648015d3dfea7969141ae
Johannes Eder <jo.eder@gmx.at>
2015-01-08 15:01:30 +0100

    BenchmarkTest schreibt "Start" als Start Signal in den Benchmark Container und entfernt es wieder als Stop Signal

--- src/main/java/org/falafel/BenchmarkTest.java
+++ src/main/java/org/falafel/BenchmarkTest.java
@@ -1,6 +1,18 @@
 package org.falafel;
 
+import org.mozartspaces.capi3.AnyCoordinator;
+import org.mozartspaces.core.Capi;
+import org.mozartspaces.core.ContainerReference;
+import org.mozartspaces.core.DefaultMzsCore;
+import org.mozartspaces.core.Entry;
+import org.mozartspaces.core.MzsConstants;
+import org.mozartspaces.core.MzsCore;
+import org.mozartspaces.core.MzsCoreException;
+import org.slf4j.Logger;
+
 import java.net.URI;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Thread to control the benchmark test.
@@ -10,6 +22,8 @@ public class BenchmarkTest extends Thread {
     private final URI spaceUri;
     /** Length of the Test in ms. */
     private static final int TEST_TIME = 60000;
+    /** Get the Logger for the current class. */
+    private static final Logger LOGGER = getLogger(BenchmarkTest.class);
 
     /**
      * Create the benchmark test with the URI of the mozart space.
@@ -25,12 +39,42 @@ public class BenchmarkTest extends Thread {
      * Start the benchmark test.
      */
     public final void run() {
-        System.out.println("Start of the Benchmark!");
+        ContainerReference container;
+        MzsCore core = DefaultMzsCore.newInstanceWithoutSpace();
+        Capi capi = new Capi(core);
+
+        try {
+            container = capi.lookupContainer("benchmark", spaceUri,
+                    MzsConstants.RequestTimeout.TRY_ONCE, null);
+        } catch (MzsCoreException e) {
+            LOGGER.error("Benchmark can't find the container!");
+            return;
+        }
+
+        try {
+            capi.write(container, MzsConstants.RequestTimeout.ZERO,
+                    null, new Entry("Start"));
+        } catch (MzsCoreException e) {
+            LOGGER.error("Benchmark can't write to the container!");
+            return;
+        }
+
+        LOGGER.error("Start of the Benchmark!");
         try {
             Thread.sleep(TEST_TIME);
         } catch (InterruptedException e) {
             System.out.println("Benchmark test sleep disturbed!");
         }
-        System.out.println("End of the Benchmark!");
+
+        try {
+            capi.take(container,
+                    AnyCoordinator.newSelector(),
+                    MzsConstants.RequestTimeout.ZERO,
+                    null);
+        } catch (MzsCoreException e) {
+            LOGGER.error("Benchmark can't take from the container!");
+        }
+
+        LOGGER.error("End of the Benchmark!");
     }
 }
